5ab147bcad89c9c114d334590293dcc8d7e9e707
Joy Bhattacharjee <joy.bhattacharjee@sap.com>
2022-07-09 19:48:59 +0530

    Update knapsack 0/1 top down

--- src/algo/dynamic/knapsack/topdown/Knapsack.java
+++ src/algo/dynamic/knapsack/topdown/Knapsack.java
@@ -2,43 +2,51 @@ package algo.dynamic.knapsack.topdown;
 
 class Knapsack {
 
-  public int solveKnapsack(int[] profits, int[] weights, int capacity) {
-    Integer[][] dp = new Integer[profits.length][capacity + 1];
-    return this.knapsackRecursive(dp, profits, weights, capacity, 0);
-  }
+	public int solveKnapsack(int[] profits, int[] weights, int capacity) {
 
-  private int knapsackRecursive(Integer[][] dp, int[] profits, int[] weights, int capacity,
-      int currentIndex) {
+		Integer[][] dp = new Integer[profits.length][capacity + 1];
+		return knapsackRecursive(dp, profits, weights, capacity, 0);
 
-    // base checks
-    if (capacity <= 0 || currentIndex >= profits.length)
-      return 0;
+	}
 
-    // if we have already solved a similar problem, return the result from memory
-    if(dp[currentIndex][capacity] != null)
-      return dp[currentIndex][capacity];
+	private int knapsackRecursive(Integer[][] dp, int[] profits, int[] weights, int capacity, int currentIndex) {
 
-    // recursive call after choosing the element at the currentIndex
-    // if the weight of the element at currentIndex exceeds the capacity, we shouldn't process this
-    int profit1 = 0;
-    if( weights[currentIndex] <= capacity )
-        profit1 = profits[currentIndex] + knapsackRecursive(dp, profits, weights,
-                capacity - weights[currentIndex], currentIndex + 1);
+		// base check
+		if (capacity <= 0 || currentIndex >= profits.length) {
+			return 0;
+		}
 
-    // recursive call after excluding the element at the currentIndex
-    int profit2 = knapsackRecursive(dp, profits, weights, capacity, currentIndex + 1);
+		//check if the calculated value is present in the memory 
+		//then return from the memory 
+		if (dp[currentIndex][capacity] != null) {
+			return dp[currentIndex][capacity];
+		}
 
-    dp[currentIndex][capacity] = Math.max(profit1, profit2);
-    return dp[currentIndex][capacity];
-  }
+		int profitIncluded = 0;
 
-  public static void main(String[] args) {
-    Knapsack ks = new Knapsack();
-    int[] profits = {1, 6, 10, 16};
-    int[] weights = {1, 2, 3, 5};
-    int maxProfit = ks.solveKnapsack(profits, weights, 7);
-    System.out.println("Total knapsack profit ---> " + maxProfit);
-    maxProfit = ks.solveKnapsack(profits, weights, 6);
-    System.out.println("Total knapsack profit ---> " + maxProfit);
-  }
+		//recursive call to include the item
+		if (weights[currentIndex] < capacity) {
+			profitIncluded = profits[currentIndex]
+					+ knapsackRecursive(dp, profits, weights, capacity - weights[currentIndex], currentIndex + 1);
+		}
+
+		//recursive call to skip the item
+		int profitNotIncluded = knapsackRecursive(dp, profits, weights, capacity, currentIndex + 1);
+
+		dp[currentIndex][capacity] = Math.max(profitIncluded, profitNotIncluded);
+
+		return dp[currentIndex][capacity];
+
+	}
+
+	public static void main(String[] args) {
+		Knapsack ks = new Knapsack();
+		int[] profits = { 1, 6, 10, 16 };
+		int[] weights = { 1, 2, 3, 5 };
+		int maxProfit = ks.solveKnapsack(profits, weights, 7);
+		System.out.println("Total knapsack profit ---> " + maxProfit);
+		maxProfit = ks.solveKnapsack(profits, weights, 6);
+		System.out.println("Total knapsack profit ---> " + maxProfit);
+	}
+
 }
